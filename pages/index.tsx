f7ce257e479dc257d1663d3f7c10a79c0b6b8fb1
Semilore Idowu <semijacks27@gmail.com>
2022-05-13 12:29:15 +0100

    preload fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,38 @@ import Hero from '../components/hero';
 const Home: NextPage = () => {
   return (
     <div className='min-h-screen px-12'>
+      <Head>
+        <link
+          rel='preload'
+          href='/fonts/Exo/Exo-Bold.woff2'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/Exo/Exo-Medium.woff2'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/Exo/Exo-Regular.woff2'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/Exo/Exo-SemiBold.woff2'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/Modern_Antiqua/ModernAntiqua-Regular.woff2'
+          as='font'
+          crossOrigin=''
+        />
+      </Head>
       <Header />
       <Hero />
     </div>
